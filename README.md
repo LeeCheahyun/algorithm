5f9b3e19f9996f211ac596e0b4a654eaec71360c
LeeCheahyun <yuipo190@gmail.com>
2021-12-21 10:54:31 +0900

    1221

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# algorithm
+# algorithm project
+### 알고리즘 기본 연습
+test_algorithm.py: 기본구성 이해  
+test_algorithm02.py: 심화 이해  
+[this text](./project_desc/README.md)
---
+++ math_func.py
@@ -0,0 +1,19 @@
+# laser_range = [0.1, 0.2, 0.3, 0.4, 0.5]
+
+# import numpy as np
+# laser_arr = np.array(laser_range)
+
+# result = np.count_nonzero(laser_range >=0.2)
+# print(result)
+
+# ---------------------------------------------------------------
+
+import math
+
+conv_radian = math.radians(90)
+
+conv_degrees = math.degrees(conv_radian)
+
+print(conv_radian, ' , ' ,conv_degrees)
+
+# 90 = 1.5707963267948966
